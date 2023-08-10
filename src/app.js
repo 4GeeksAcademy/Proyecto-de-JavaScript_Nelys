c70c0d1c620fa710a9b0fd5128a27c08b2c9c26c
NelysT <139475672+NelysT@users.noreply.github.com>
2023-08-10 21:18:30 +0000

    Pasando los cambios a mi branch

--- src/app.js
+++ src/app.js
@@ -7,5 +7,22 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "His turtle", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "the keys", "the car"];
+  let when = [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  var ind_who = (Math.random() * 3).toFixed(0);
+  var ind_action = (Math.random() * 3).toFixed(0);
+  var ind_what = (Math.random() * 3).toFixed(0);
+  var ind_when = (Math.random() * 3).toFixed(0);
+  document.getElementById("who").innerHTML = who[ind_who];
+  document.getElementById("action").innerHTML = action[ind_action];
+  document.getElementById("what").innerHTML = what[ind_what];
+  document.getElementById("when").innerHTML = when[ind_when];
 };
