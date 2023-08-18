defa9161901d6d55ce99aa58499baedd9c4606fd
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-18 18:35:27 +0000

    Cambios en el archivo js

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let who = ["The dog", "My grandma", "His turtle", "My bird"]; //holaa
+  let who = ["The dog", "My grandma", "His turtle", "My bird"]; 
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
   let when = [
@@ -17,9 +17,9 @@ window.onload = function() {
     "during my lunch",
     "while I was praying"
   ];
-  var ind_who = (Math.random() * 3).toFixed(0);
-  var ind_action = (Math.random() * 3).toFixed(0);
-  var ind_what = (Math.random() * 3).toFixed(0);
+  var ind_who = (Math.random() * 3).toFixed(0); //ind no es un nombre semántico, mejor index.
+  var ind_action = (Math.random() * 3).toFixed(0); //toFixed() además de quitar los decimales, convierte el número en string. No es aconsejable, mejor Math.floor().
+  var ind_what = (Math.random() * 3).toFixed(0); //no es aconsejable utilizar var al estar en desuso, mejor declarar la variable con let.
   var ind_when = (Math.random() * 3).toFixed(0);
   document.getElementById("who").innerHTML = who[ind_who];
   document.getElementById("action").innerHTML = action[ind_action];
