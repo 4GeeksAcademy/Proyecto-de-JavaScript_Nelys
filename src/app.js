7c5e06b52db720437859268f524dbb0acb1bf20d
flaviaolivera <126910577+flaviaolivera@users.noreply.github.com>
2023-08-18 18:26:58 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let who = ["The dog", "My grandma", "His turtle", "My bird"]; //hola
+  let who = ["The dog", "My grandma", "His turtle", "My bird"]; //holaa
   let action = ["ate", "peed", "crushed", "broke"];
   let what = ["my homework", "the keys", "the car"];
   let when = [
